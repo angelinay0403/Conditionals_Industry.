3fe8d6d6e732bdf2976f51cc4893b0b4ab11f690
angelinay0403 <angelinay0403@gmail.com>
2013-11-14 20:04:59 -0500

    begining of the assigment

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-// JavaScript Document
+// Angelina Castro, 11/13/2013, conditional.
+
+//Conditionals_Industry.
+
+// Better food's supermarket has a total budget of 5000 dollars a week for payroll of 10 employees, if they hire new people the supermarket is going under budget
+// and have to start cutting hours.                      
