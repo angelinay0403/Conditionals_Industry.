3134fc23add5508a099830bec4374a5c7789bc79
angelinay0403 <angelinay0403@gmail.com>
2013-11-14 21:12:34 -0500

    fixed all the errors

--- js/script.js
+++ js/script.js
@@ -3,4 +3,19 @@
 //Conditionals_Industry.
 
 // Better food's supermarket has a total budget of 5000 dollars a week for payroll of 10 employees, if they hire new people the supermarket is going under budget
-// and have to start cutting hours.                      
+// and have to start cutting hours.
+
+var budget = 5000
+var payCheck = 500
+var employees = 10
+var newHire = 4
+
+// if i have 10 employees times paycheck is the same as budget.
+	if(employees * payCheck >= budget){
+	console.log("Better food's supermarket has the right amount for payroll")
+	
+}else if(employees + newHire * payCheck > budget){
+	console.log("Better food's supermarket needs to cut hours!")
+}
+
+                     
